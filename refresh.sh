c0b0183eeb10ada8ddaa1c73fa41b93183297de0
Martin Doms <iamaelephant@gmail.com>
2022-02-09 07:39:56 +1300

    Automatic data update

--- refresh.sh
+++ refresh.sh
@@ -6,7 +6,7 @@ pushd ~/src/motogp-no-spoiler
 git pull origin master
 #DOTNET_ROOT=/home/pi/dotnet-arm32/ /home/pi/dotnet-arm32/dotnet run
 dotnet run
-cp -r ./output ~/nginx/config/www
+cp -r ./output/* ~/nginx/config/www
 
 git add .
 git commit -m "Automatic data update"
